8c366ed2c643f4f8eeadb79d88d258bbd972ad95
Marshall Bowers <elliott.codes@gmail.com>
2015-05-21 20:36:35 -0400

    Add $expand, $top, and $orderby

--- dist/odata.js
+++ dist/odata.js
@@ -16,6 +16,7 @@ var OData = (function () {
 
     var _url = '';
     var _select = [];
+    var _expand = [];
 
     var OData = function () {
 
@@ -37,15 +38,98 @@ var OData = (function () {
 
     };
 
+    OData.prototype.expand = function (item) {
+
+        _expand.push(item);
+
+        return this;
+
+    }
+
     OData.prototype.build = function () {
 
-        return _url + '?' + buildSelect();
+        var query = '';
+
+        var params = getParams();
+
+        console.log(params)
+
+        for (var i in params) {
+            if (!i) {
+                continue;
+            }
+
+            if (!params[i]) {
+                continue;
+            }
+
+            query += i + '=' + params[i] + '&';
+        }
+
+        if (query.lastIndexOf('&') === query.length - 1) {
+            query = query.slice(0, query.length - 1);
+        }
+
+        return query;
+
+        return _url + '?' + buildSelect() + buildExpand();
+
+    };
+
+    var getParams = function () {
+
+        var params = {};
+        var current = {};
+
+        current = buildSelect();
+
+        params[current.key] = current.value;
+
+        current = buildExpand();
+
+        params[current.key] = current.value;
+
+        return params;
 
     };
 
     var buildSelect = function () {
 
-        return '$select={0}'.format(_select.join(','));
+        var parameter = {
+            key: '',
+            value: ''
+        };
+
+        if (_select.length < 1) {
+            return parameter;
+        }
+
+        parameter = {
+            key: '$select',
+            value: _select.join(',')
+        };
+
+        return parameter;
+
+    };
+
+    var buildExpand = function () {
+
+        var parameter = {
+            key: '',
+            value: ''
+        };
+
+        if (_expand.length < 1) {
+            return parameter;
+        }
+
+        parameter = {
+            key: '$expand',
+            value: _expand.join(',')
+        };
+
+        return parameter;
 
     };
 
